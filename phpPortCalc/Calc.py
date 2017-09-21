9852ecb6f66243cd35b2572c3ae5befd617f35b4
Cody <Codyegrimes@outlook.com>
2017-09-21 18:37:37 -0400

    Calculate function split into 4 single-purpose functions

--- phpPortCalc/Calc.py
+++ phpPortCalc/Calc.py
@@ -20,24 +20,61 @@ def getInput():
 #startingSR, targetSR = getInput()
 	
 	
+	
 def calculate(currentTierPrice, targetSR, price, tierCieling):
+	#DEPRECATED: has been split into four methods
 	tierSR = targetSR - tierCieling
 	priceForTier = tierSR * currentTierPrice
 	price += priceForTier
 	targetSR -= tierSR
 	return currentTierPrice, targetSR, price
+	
+	
+	
+def calcTierSR(targetSR, tierCieling): #determines how much SR to be price at this tier
+	tierSR = targetSR - tierCieling
+	return tierSR
+	
+def priceForTier(tierSR, currentTierPrice): #determines what the price for this tier is
+	priceForTier = tierSR * currentTierPrice
+	return priceForTier
+	
+def adjustTargetSR(targetSR, tierSR): #adjusts targetSR before next call
+	targetSR -= tierSR
+	return targetSR
 
-
+def accumulatePrice(price, priceForTier): #think of better function name; adds to running total
+	price += priceForTier
+	return price
 	
 	
 	
-def recursiveCalc(startingSR, targetSR, price, currentTierPrice): #price/currentTier start at 0, get replaced by actual value...
+	
+def recursiveCalc(startingSR, targetSR, price): #todo revise: price/currentTier start at 0, get replaced by actual value...
+	if(targetSR>4450):
+		currentTierPrice = 2.8
+		tierSR = calcTierSR(targetSR, 4450)
+		priceInTier = priceForTier(tierSR, currentTierPrice)
+		price = accumulatePrice(price, priceInTier)
+		targetSR = adjustTargetSR(targetSR, tierSR)
+	elif(targetSR>4400):
+		currentTierPrice = 2.6
+		tierSR = calcTierSR(targetSR, 4400)
+		priceInTier = priceForTier(tierSR, currentTierPrice)
+		price = accumulatePrice(price, priceInTier)
+		targetSR = adjustTargetSR(targetSR, tierSR)
+	elif(targetSR>4350):
+		currentTierPrice = 2.4
+		tierSR = calcTierSR(targetSR, 4350)
+		priceInTier = priceForTier(tierSR, currentTierPrice)
+		price = accumulatePrice(price, priceInTier)
+		targetSR = adjustTargetSR(targetSR, tierSR)
+		
 	if(targetSR<=startingSR):
 		return price - ((startingSR - targetSR) * currentTierPrice)
-	elif(targetSR>4450):
-		currentTierPrice = 2.8
-		currentTierPrice, targetSR, price = calculate(2.8, targetSR, price, 4450)
-		return recursiveCalc(startingSR, targetSR, price, currentTierPrice)
+	else:
+		return recursiveCalc(startingSR, targetSR, price)
+
 
 	
 	
@@ -46,12 +83,13 @@ def recursiveCalc(startingSR, targetSR, price, currentTierPrice): #price/current
 	
 	
 price = 0
-currentTierPrice = 0	
+
 
 #run calculator --> what the user sees
 def calc(startingSR, targetSR):
-	return recursiveCalc(startingSR,targetSR, price, currentTierPrice) #INTERNAL call with other params
+	return recursiveCalc(startingSR,targetSR, price) #INTERNAL call with other params
 	
-result = calc(4451, 4455)
+#hard coding for now in order to test
+result = calc(4370, 4401)
 print (result)
 	
